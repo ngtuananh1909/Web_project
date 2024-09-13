8d66d1a1dad5847ef8ddd0c160db432d76fd1055
nta <nta1909k7@gmail.com>
2024-09-13 15:15:04 +0700

    near winner

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
