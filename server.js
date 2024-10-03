9e4b221599da555ef04ca520773d9efec53c3952
phattpro <tatanphat2008@gmail.com>
2024-10-03 21:32:38 +0700

    yes yes yes

--- server.js
+++ server.js
@@ -8,61 +8,85 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const homeRouter = require('./routes/home.router.js');
 const productRouter = require('./routes/product.router.js');
-const paymentRouter = require('./routes/payment.router.js');
+const paymentRouter = require('./routes/payment.router.js')
 const fs = require('fs');
-const https = require('https');
-
 dotenv.config();
-
 const app = express();
-const options = {
-    key: fs.readFileSync('./ssl/private.key'),
-    cert: fs.readFileSync('./ssl/certificate.crt')
-};
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// Middleware
-app.use(fileUpload({ createParentPath: true }));
-app.use(helmet());
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            imgSrc: ["'self'", "http://localhost:3000", "data:"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
+            styleSrc: ["'self'", "'unsafe-inline'"]
+        }
+    },
+    frameguard: {
+        action: 'deny'
+    },
+    xssFilter: true,
+    noSniff: true
+}));
+
 const corsOptions = {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 };
+app.locals.formatCurrency = function(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+  
 app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.use(express.static('public')); 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static('event_function'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(session({
     secret: 'uit2027goo',
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false, // Set to true in production
-        sameSite: 'lax'
+        secure: false,
+        sameSite: 'lax' 
     }
 }));
+
 app.use((req, res, next) => {
     res.locals.session = req.session;
     next();
 });
 
-const httpsServer = https.createServer(options, app);
-const io = require('socket.io')(httpsServer);
-
-io.on('connection', (socket) => {
-    socket.on('chatMessage', (from, msg) => {
-        io.emit('chatMessage', from, msg);
-    });
-    socket.on('notifyUser', (user) => {
-        io.emit('notifyUser', user);
-    });
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', "img-src 'self' http://localhost:3000 data:;");
+    next();
 });
+
+io.on('connection', function(socket){
+    socket.on('chatMessage', function(from, msg){
+      io.emit('chatMessage', from, msg);
+    });
+    socket.on('notifyUser', function(user){
+      io.emit('notifyUser', user);
+    });
+  });
+
 
 app.use('/', homeRouter);
 app.use('/product', productRouter);
@@ -70,7 +94,6 @@ app.use('/payment', paymentRouter);
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 const PORT = process.env.PORT || 3000;
-
-httpsServer.listen(PORT, () => {
-    console.log(`Server is running on https://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
