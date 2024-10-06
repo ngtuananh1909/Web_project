2d03248701121c39e476dbe1d65a169733f93e91
phattapro <tatanphat2008@gmail.com>
2024-10-07 06:44:40 +0700

    ez

--- connect/database.js
+++ connect/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: localhost,
+  host: 'localhost',
+  user: 'root',
   port: 3001,
   password: '',
-  database: product_db,
+  database: 'product_db',
   waitForConnections: true,
   connectionLimit: 5, // Thay đổi tùy theo nhu cầu của bạn
   queueLimit: 0
