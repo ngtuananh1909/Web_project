37a363a82db1e8f531e38fa071965534986d2ec0
phattpro <tatanphat2008@gmail.com>
2024-10-06 23:11:33 +0700

    ez

--- server.js
+++ server.js
@@ -8,14 +8,24 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const homeRouter = require('./routes/home.router.js');
 const productRouter = require('./routes/product.router.js');
-const paymentRouter = require('./routes/payment.router.js')
+const paymentRouter = require('./routes/payment.router.js');
 const fs = require('fs');
+
 dotenv.config();
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
+
+// Kết nối Redis
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD // Nếu cần mật khẩu
+});
+
+redisClient.on('error', (err) => {
+    console.error('Redis error:', err);
+});
 
 app.use(fileUpload({
     createParentPath: true
@@ -43,12 +53,12 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 };
+
 app.locals.formatCurrency = function(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  };
-  
-app.use(cors(corsOptions));
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -56,25 +66,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public')); 
 
-app.use(express.static('event_function'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
 app.use(session({
-    store: new RedisStore({ client: redisClient }),
     secret: 'truepablo',
     resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true } 
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 } 
   }));
 
 app.use((req, res, next) => {
     res.locals.session = req.session;
-    next();
-});
-
-app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "img-src 'self' http://localhost:3000 data:;");
     next();
 });
 
@@ -85,8 +85,7 @@ io.on('connection', function(socket){
     socket.on('notifyUser', function(user){
       io.emit('notifyUser', user);
     });
-  });
-
+});
 
 app.use('/', homeRouter);
 app.use('/product', productRouter);
@@ -94,6 +93,6 @@ app.use('/payment', paymentRouter);
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
